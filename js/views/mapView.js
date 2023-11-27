66ee9c6aed6cd5fbf63023ba408d475b1cef43b9
Natália Veras <verasnatali4@gmail.com>
2023-11-27 23:14:36 +0100

    Added custom marker

--- js/views/mapView.js
+++ js/views/mapView.js
@@ -1,20 +1,31 @@
 import View from "./View.js";
 import { ZOOM_LEVEL } from "../config.js";
 import L from "leaflet";
+import customPinImage from "../../images/icon-location.svg";
 class MapView extends View {
   _parentElement = document.querySelector("#map");
+  _pinText = "IP address location";
   _map;
 
-  initMap(coords) {
+  async initMap({ lat, lng }) {
     try {
       this._map = L.map("map", {
-        center: [coords.lat, coords.lng],
+        center: [lat, lng],
         zoom: ZOOM_LEVEL,
       });
 
       L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         maxZoom: ZOOM_LEVEL,
-      }).addTo(this._map);
+      });
+
+      const icon = L.icon({
+        iconUrl: customPinImage,
+        iconSize: [32, 40],
+        iconAnchor: [16, 32],
+        popupAnchor: [0, -32],
+      });
+
+      L.marker([lat, lng], { icon }).addTo(this._map).bindPopup(this._pinText);
     } catch (error) {
       throw new Error(
         `There was an issue initializing the map: ${error.message}`
